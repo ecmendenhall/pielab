64dd9de2d573516e2014a61fc69b507f6ab6f58f
Connor Mendenhall <ecmendenhall@gmail.com>
2013-03-28 18:07:23 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pielab::Application.config.secret_token = 'bceffdbee132c23c22b99dd367b82412c17308f7ed062a5337e3a3861647af09d0dbf003f4dc75dbf8b615855419f3ec1e8eedce8c0c1dc77762452b03ccf7d1'
