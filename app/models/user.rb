5fb924eb0491b4e52af459cd45f8c5ccca999b55
Connor Mendenhall <ecmendenhall@gmail.com>
2013-03-30 14:49:26 -0700

    Finished login.

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,10 +24,16 @@ class User < ActiveRecord::Base
   validates :password_confirmation, :presence => true
 
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 
   VALID_EMAIL_REGEX = /[\w+\-.]+@[a-z\d\-.]+\.[a-zA-Z]+/i
   validates :email, 
       :presence => true, 
       :format => { :with => VALID_EMAIL_REGEX },
       :uniqueness => { :case_sensitive => false }
+
+  private
+      def create_remember_token
+          self.remember_token = SecureRandom.urlsafe_base64
+      end
 end
